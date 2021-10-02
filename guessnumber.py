44b2c3d64f11ccc6262c7998ecfa5c89b259b6e4
Yiting Lin <bestfriend830@gmail.com>
2021-10-03 00:12:51 +0800

    decide by user

--- guessnumber.py
+++ guessnumber.py
@@ -1,11 +1,13 @@
 import random
 
-answer = random.randint(1, 100)
+start = int(input('請決定隨機數開始值： '))
+end = int(input('請決定隨機數結束值： '))
+answer = random.randint(start, end)
 
 count = 1
 while True:
 	print('開始第', count, '次猜測')
-	guess = int(input('請猜一個數字(1~100): '))
+	guess = int(input('請猜一個數字: '))
 	if guess == answer:
 		print('猜對了！')
 		break
